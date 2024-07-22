114b67d4b814ba9c54f834980f24a9f8bde2bc7c
Christophe Jolas <christophe.jolas@gmail.com>
2024-07-22 09:54:40 +0200

    [init] project

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Counter from "./Counter";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/counter" element={<Counter />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/client/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p>Aller à la page SSR et hydratée <a href="/counter">de manière interactive</a></p>
+    </div>
+  );
+};
+
+export default Home;
